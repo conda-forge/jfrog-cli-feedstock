a0dd0a5001141c232bff7528b77dd212b1ed88ae
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-09-24 14:57:16 -0500

    update ignored licenses

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,9 @@
 
 cd %SRC_DIR%
 go build -ldflags "-X main.revision=conda-forge" -v -o %LIBRARY_PREFIX%\bin\jf.exe
-go-licenses save . --ignore "github.com/xi2/xz" --save_path .\library_licenses
+go-licenses save . ^
+    --ignore "github.com/cyberphone/json-canonicalization" ^
+    --ignore "github.com/in-toto/attestation" ^
+    --ignore "github.com/in-toto/in-toto-golang" ^
+    --ignore "github.com/xi2/xz" ^
+    --save_path .\library_licenses
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,9 @@ set -ex
 
 cd $SRC_DIR
 go build -ldflags "-X main.revision=conda-forge" -v -o $PREFIX/bin/jf
-go-licenses save . --ignore "github.com/xi2/xz" --save_path ./library_licenses
+go-licenses save . \
+    --ignore "github.com/cyberphone/json-canonicalization" \
+    --ignore "github.com/in-toto/attestation" \
+    --ignore "github.com/in-toto/in-toto-golang" \
+    --ignore "github.com/xi2/xz" \
+    --save_path ./library_licenses
